86c2c3bab93f7d44c40ee110c31838ce9eabc2a0
MarioGranada <mario.granada190@gmail.com>
2024-02-01 06:38:02 -0800

    feat timers with settimeout

---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,31 @@
+import { forwardRef, useImperativeHandle, useRef } from 'react';
+
+const ResultModal = forwardRef(({ result, targetTime }, ref) => {
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        // Expose open method to outter/external components
+        dialog.current.showModal();
+      },
+    };
+  });
+
+  return (
+    <dialog ref={dialog} className="result-modal">
+      <h2>You {result}</h2>
+      <p>
+        The target time was <strong>{targetTime} seconds </strong>{' '}
+      </p>
+      <p>
+        You stopped the timer with <strong>X seconds left.</strong>{' '}
+      </p>
+      <form method="dialog">
+        <button>close</button>
+      </form>
+    </dialog>
+  );
+});
+
+export default ResultModal;
---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,42 @@
+import { useRef, useState } from 'react';
+import ResultModal from './ResultModal';
+
+const TimerChallenge = ({ title, targetTime }) => {
+  const timer = useRef();
+  const dialog = useRef();
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+  const handleStart = () => {
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+      dialog.current.open();
+    }, targetTime * 1000);
+    setTimerStarted(true);
+  };
+
+  const handleStop = () => {
+    clearTimeout(timer.current);
+  };
+
+  return (
+    <>
+      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+      <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? 's' : ''}
+        </p>
+        <p>
+          <button onClick={timerStarted ? handleStop : handleStart}>
+            {timerStarted ? 'Stop' : 'Start'} Challenge
+          </button>
+        </p>
+        <p className={timerStarted ? 'active' : undefined}>
+          {timerStarted ? 'Timer is running...' : 'Timer inactive'}
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default TimerChallenge;
